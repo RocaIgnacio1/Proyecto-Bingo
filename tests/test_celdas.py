099ad1655af8f987c2802ae08d6a98b46f63978e
nacho <nacho@example.com>
2020-04-20 18:19:18 -0300

    Nuevo test: verificar que en cada columna, por lo menos haya una celda ocupada

--- tests/test_celdas.py
+++ tests/test_celdas.py
@@ -1,4 +1,5 @@
 from src.bingo import carton
+from src.bingo import columnas
 
 def test_contar_celdas_ocupadas():
     mi_carton = carton()
@@ -8,3 +9,15 @@ def test_contar_celdas_ocupadas():
             contador = contador + celda
 
     assert contador == 15
+
+
+def test_al_menos_una_celda_ocupada_por_columna():
+    mi_carton = carton()
+    contador_columnas = 0
+    contador = 0
+    for contador_columnas in range(9):
+        mi_columna = columnas(mi_carton,contador_columnas)
+        if sum(mi_columna) >= 1:
+            contador = contador + 1
+
+    assert contador == 9
