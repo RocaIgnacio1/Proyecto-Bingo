c3b384c2426feff23824ec7ce9ec42f4db53c90c
nacho <nacho@example.com>
2020-06-15 19:39:21 -0300

    Nuevo test: En una fila no existen más de dos celdas ocupadas consecutivas

---
+++ tests/test_no_existen_masde2_celdas_consecutivas_ocupadas.py
@@ -0,0 +1,10 @@
+from src.bingo import carton
+
+def test_en_una_fila_no_existen_masde2_celdas_ocupadas_consecutivas():
+    mi_carton = carton()
+    for fila in range(3):
+        for columna in range(7):
+            check = 0
+            if mi_carton[fila][columna] == 0 and mi_carton[fila][columna+1] == 0 and mi_carton[fila][columna+2] == 0:
+                check = check + 1
+            assert check == 0
